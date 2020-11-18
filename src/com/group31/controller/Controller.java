8470921fa7b08823c663572c1b06f2b510710f6a
liamdp <liam.pickering@outlook.com>
2020-11-18 12:39:33 +0000

    added skeleton of controller, main. Created settings

---
+++ src/com/group31/controller/Controller.java
@@ -0,0 +1,37 @@
+package com.group31.controller;
+
+public class Controller {
+
+    private Player[] players;
+    private Gameboard gameboard;
+    private Silkbag silkbag;
+    private int[] permittedPlacePoint;
+    private FileManager fileManager;
+
+    public Controller(Player[] players, Gameboard gameboard, Silkbag silkbag) {
+        this.players = players;
+        this.gameboard = gameboard;
+        this.silkbag = silkbag;
+    }
+
+    public Tile takeTile(Player player) {
+        // give tile to player from silkbag
+    }
+
+    public void movePlayer(int x, int y) {
+        // move a player on a board
+    }
+
+    public void drawTileOnBoard(Tile tile, int x, int y) {
+        // draw a tile on the gameboard
+    }
+
+    public boolean validatePlayerMove(Player player, int x, int y) {
+        // validate a player move, consider creating a class for validating?
+    }
+
+    public void saveGame() {
+        // save the game passing info to the file reader
+    }
+
+}
--- src/com/group31/main/Main.java
+++ src/com/group31/main/Main.java
@@ -1,9 +1,42 @@
 package com.group31.main;
 
+import com.group31.controller.Controller;
+import com.group31.settings.Settings;
+
 public class Main {
 
+    Controller controller;
+
     public static void main(String[] args) {
-        // start program here
+
+        // init settings from a file
+
+        if (args.length == 0) {
+            Settings.updateSettings(args);
+        }
+    }
+
+    private static Leaderboard initLeaderBoard() {
+        // load stats, initialises, pass back
+    }
+
+    private static Silkbag initSilkBag() {
+        // loads max tiles, tiles inside if save game etc, initialises with tiles, creates new instance, pass back
+    }
+
+    private static Gameboard initGameboard() {
+        // loads size, saved state if any, initialises, pass back
+    }
+
+    private static Player initPlayers() {
+        // loads players if any saved or creates new players (however many are asked for at runtime), pass back
+    }
+
+    private static void initController(Player[] players,
+                                       Gameboard gameboard,
+                                       Silkbag silkbag,
+                                       Leaderboard leaderboard) {
+        // from here go to the controller, don't come back to main.
     }
 
 }
