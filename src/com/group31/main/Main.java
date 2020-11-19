0112670bf87d644187f8574f874c01cd662db1fc
Liam Pickering <liam.pickering@outlook.com>
2020-11-19 14:12:17 +0000

    Update Main.java

--- src/com/group31/main/Main.java
+++ src/com/group31/main/Main.java
@@ -1,12 +1,10 @@
-/**
- * Initialises all components and starts app.
- */
 package com.group31.main;
 
 public final class Main {
 
     /**
      * Initialises the components and runs the app.
+     * @param args Args passed in at runtime.
      */
     public static void main(final String[] args) {
         // start program here
