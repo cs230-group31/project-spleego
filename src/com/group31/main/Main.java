a4d3795bc19a7dbeb8bcab2ea86e401ac6a3897e
Abdullah Shahid <55884757+MoeShahid@users.noreply.github.com>
2020-11-22 18:44:13 +0000

    Added Message of The Day (#9)
    * Added API Request
    
    * finished puzzle solver, now able to get motd
    
    * added javadoc, fixed linting errors
    
    * fixed more linting errors
    
    * Created shellscript to compile all java classes
    
    * Renamed shellscript file
    
    * Delete compileAll.sh
    
    Co-authored-by: liamdp <liam.pickering@outlook.com>

--- src/com/group31/main/Main.java
+++ src/com/group31/main/Main.java
@@ -1,12 +1,35 @@
 package com.group31.main;
 
-public final class Main {
+import com.group31.services.ApiRequest;
+import com.group31.services.PuzzleSolver;
+
+public class Main {
 
     /**
      * Initialises the components and runs the app.
+     *
      * @param args Args passed in at runtime.
      */
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         // start program here
+
+        // Testing:
+        System.out.println(getMotd("http://cswebcat.swansea.ac.uk/", "puzzle", "message",
+                "?solution="));
+    }
+
+    /**
+     * Gets the MOTD.
+     * @param urlBase URL base for the API.
+     * @param puzzleRoute The URL route of the puzzle text.
+     * @param messageRoute The URL route of the message text.
+     * @param tokenIdentifier The tag in the URL that identifies the token.
+     * @return The response from the API as a string.
+     */
+    private static String getMotd(String urlBase, String puzzleRoute, String messageRoute,
+                                  String tokenIdentifier) {
+        ApiRequest request = new ApiRequest(urlBase, puzzleRoute);
+        String puzzle = PuzzleSolver.solvePuzzle(request.getResponse());
+        return new ApiRequest(urlBase, messageRoute, puzzle, tokenIdentifier).getResponse();
     }
 }
