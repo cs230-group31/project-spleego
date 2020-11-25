65b90ba4a07bb1d97f0c8b8687a8e147445fdb4d
aarontf <aprewett1999@gmail.com>
2020-11-25 14:27:02 +0000

    Update Tile.java

--- src/com/group31/tileManager/Tile.java
+++ src/com/group31/tileManager/Tile.java
@@ -1,11 +1,5 @@
 package com.group31.tileManager;
-
-import com.group31.logger.Logger;
 import javafx.scene.image.Image;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 
 
@@ -30,30 +24,16 @@ public class Tile {
     /**
      * Identifies the tile.
      */
-    private int id;
+    private final int id;
 
     /**
      * Class constructor.
      * @param id identifies the tile
+     * @param currentImage the image of the tile
      */
-    public Tile(int id) {
+    public Tile(int id, Image currentImage) {
         this.id = id;
-        Image tileImg = null;
-        File dir = new File(TILES_LOCATION);
-        File[] directoryListing = dir.listFiles();
-        if (directoryListing != null) {
-            for (File file : directoryListing) {
-                try {
-                    tileImg = new Image(new FileInputStream(file.getPath()));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                TILE_IMAGES.put(Integer.parseInt(file.getName().replaceFirst("[.][^.]+$", "")), tileImg);
-            }
-        } else {
-            Logger.log("Directory not found", Logger.Level.ERROR);
-        }
-        currentImage = TILE_IMAGES.get(id);
+        this.currentImage = currentImage;
     }
 
     /**
