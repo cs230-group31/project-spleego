925fd54e41bc0544495122b38c2ac57bcf09e518
aarontf <aprewett1999@gmail.com>
2020-11-26 13:52:54 +0000

    Update GoalTile.java
    The Goal Tile should do literally nothing except exist in a specific space, so I have updated it to represent this.

--- src/com/group31/tile_manager/GoalTile.java
+++ src/com/group31/tile_manager/GoalTile.java
@@ -4,63 +4,12 @@ import com.group31.logger.Logger;
 import javafx.scene.image.Image;
 
 /**
- * this is the goal tile
+ * This represents the Goal Tile.
  */
-public class GoalTile extends Tile{
+public class GoalTile extends FloorTile{
 
-    /**
-     * Identifies the tile.
-     */
-    private final int id;
-
-    /**
-     * holds the image of the tile.
-     */
-    private final Image currentImage;
-    /**
-     * holds the routing of the tile.
-     */
-    private final String routing;
-    /**
-     * holds the coord of the tile.
-     */
-    private int[] coord;
-
-    private final int X = 0;
-    private final int Y = 0;
-
-    public GoalTile(String routing, int id, int[] coord, Image currentImage){
+    public GoalTile(String routing, int id, int[] coord, Image currentImage) {
         super(routing, id, coord, currentImage);
-        this.routing = "abcd";
-        this.id = 0;
-        coord = new int[2];
-        this.coord[X] = coord[X];
-        this.coord[Y] = coord[Y];
-        this.currentImage = new Image("resources/images/0.png");
-    }
-
-    /**
-     * @param id Sets the id.
-     */
-    //private void setId(){
-    //    this.id = 12;
-    //}
-
-    /**
-     * //@param currentImage Sets the image.
-     */
-    //private void setImage(){
-    //    this.currentImage = new Image("resources/images/0.png");
-    //}
-
-    /*
-    
-    /**
-     * when you reach this tile you win the game
-     * @param goalReached
-     */
-    public void gameWon(boolean goalReached){
-        Logger.log("you have won", Logger.Level.INFO);
     }
 
 }
