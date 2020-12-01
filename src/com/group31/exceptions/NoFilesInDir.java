b9997d7fcfc82ed5ca6d6bc65915ff917d5ed5a3
liamdp <liam.pickering@outlook.com>
2020-12-01 11:31:14 +0000

    added Javadoc to exceptions

--- src/com/group31/exceptions/NoFilesInDir.java
+++ src/com/group31/exceptions/NoFilesInDir.java
@@ -2,10 +2,17 @@ package com.group31.exceptions;
 
 public class NoFilesInDir extends Exception {
 
+    /**
+     * There are no files in the directory.
+     * @param errorMessage Custom error message.
+     */
     public NoFilesInDir(String errorMessage) {
         super(errorMessage);
     }
 
+    /**
+     * There are no files in the directory.
+     */
     public NoFilesInDir() {
 
     }
--- src/com/group31/exceptions/ObjectNeverSerialized.java
+++ src/com/group31/exceptions/ObjectNeverSerialized.java
@@ -2,10 +2,17 @@ package com.group31.exceptions;
 
 public class ObjectNeverSerialized extends Exception {
 
+    /**
+     * The object requested to be deserialized has not been serialized.
+     * @param errorMessage Custom error message.
+     */
     public ObjectNeverSerialized(String errorMessage) {
         super(errorMessage);
     }
 
+    /**
+     * The The object requested to be deserialized has not been serialized.
+     */
     public ObjectNeverSerialized() {
 
     }
