6629fe101c9255fb221077beb6ef89fdcad55eff
aarontf <aprewett1999@gmail.com>
2020-12-02 20:19:57 +0000

    Updated constructor

--- src/com/group31/tile_manager/action_tile/ActionTile.java
+++ src/com/group31/tile_manager/action_tile/ActionTile.java
@@ -14,39 +14,4 @@ public class ActionTile extends Tile {
         super(id, true, currentImage);
         Logger.log("Action tile created", Logger.Level.INFO);
     }
-
-    // TODO: I don't think this is needed, but can add back in if it is.
-//    public boolean isAction(){
-//        return isActionTile = true;
-//    }
-
-//    /**
-//    * This class is a subclass of Tile and represents
-//    * an action tile
-//    * IMPORTANT: when player takes an action tile needs to setCoord()
-//    * because tiles inside silkbag have no coords
-//    */
-//    public class ActionTile extends Tile {
-//    /**
-//     * Uses superclass constructor
-//     * @param routing
-//     * @param id
-//     * @param coord
-//     */
-//    public ActionTile(String routing, int id, int[] coord){
-//        super(routing, id, coord, true);
-//        Logger.log("Action tile created", Logger.Level.INFO);
-//    }
-
-//    /**
-//     * Uses superclass constructor with no coords
-//     * @param routing
-//     * @param id
-//     * @param actionTile
-//     */
-//    public ActionTile(String routing, int id, boolean actionTile){
-//        super(routing, id, true);
-//       Logger.log("Action tile created", Logger.Level.INFO);
-//    }
-//    }
 }
--- src/com/group31/tile_manager/action_tile/FireTile.java
+++ src/com/group31/tile_manager/action_tile/FireTile.java
@@ -1,27 +1,17 @@
 package com.group31.tile_manager.action_tile;
 
 import com.group31.logger.Logger;
+import javafx.scene.image.Image;
 
 public class FireTile extends ActionTile {
-    /**
-     * Identifies the tile.
-     */
-    private final int id;
-
-    /**
-     * holds weight of the tile.
-     */
-    private final int weight;
 
     /**
      * Fire tile ensures no player can step on a tile that is on fire, because it's on fire.
      * @param id ID of the tile.
-     * @param weight Weight of the tile (likeliness to be pulled from the silkbag).
+     * @param currentImage Image of the tile
      */
-    public FireTile(int id, int weight) {
-        super(id, weight);
-        this.id = id;
-        this.weight = weight;
+    public FireTile(int id, Image currentImage) {
+        super(id, currentImage);
     }
 
 //    /**
